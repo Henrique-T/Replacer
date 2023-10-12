69491e22c8a5d5231008aa82c37059ebf8df9ce3
Henrique Tridapalli <henrique.tridapalli@getmore.com>
2023-10-12 19:50:43 -0300

    Add pseudo code for each of the algos

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -32,15 +32,35 @@ std::vector<std::string> &Algorithm::getReferences()
 
 void Algorithm::runAlgorithm()
 {
+	void runAlgorithm();
+
 	// FIFO
 	// For each reference in references
 	// 1. try to find that reference in PT
-	// 2. If found, go to FT to get the pageinsert to FT the necessary page
+	// 2. If found, go to FT to get the page
 	// 3. If not found, print "PAGE FAULT"
-	// 3.1 Get the first inserted page in FT and remove it
+	// 3.1 Get the first inserted page in FT and remove it.
 	// 3.2 bring the requested page from disk and insert that into FT.
-	// 3.3. Update PT mapping
+	// 3.3. Update PT mapping (meaning, set presence bits)
 
 	// LRU
+	// Each page has a counter.
+	// Every clock interruption we update the counter -> page counter = page counter + presence bit.
+	// For each reference in references
+	// 1. try to find that reference in PT.
+	// 2. if found, go to FT to get the page.
+	// 3. if not found, print "PAGE FAULT".
+	// 3.1 Get the page with the biggest counter in FT and remove it.
+	// 3.2 bring the requested page from disk and insert that into FT.
+	// 3.3 update PT mapping (meaning, set presence bits).
+	// Should we apply aging????????
+
 	// Optimal
+	// For each page in PT
+	// We lable the page according to how many refrerences it will have in the future.
+	// For each reference in references
+	// 1. try to find that reference in PT
+	// 2. If found, go to FT to get the page
+	// 3. If not found, print "PAGE FAULT"
+	// 3.1 ?
 }
